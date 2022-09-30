40d770fff0cbc0a77bcb707c924f8a001791ae05
PeterM012 <mpete3d@gmail.com>
2022-09-29 20:40:23 -0700

    add answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,18 @@ var answerSelection1 = document.getElementById("answers1")
 var answerSelection2 = document.getElementById("answers2") 
 var answerSelection3 = document.getElementById("answers3") 
 var answerSelection4 = document.getElementById("answers4") 
-var answerSelection3 = document.getElementById("answers5") 
-var answerSelection4 = document.getElementById("answers6") 
+
 
 var currentQuestion = 0; //this will need to be increase by 1
 
-var questionHeaders = ["What does RGB stand for?","What are Pseudo Classes?","What are JavaScript Date Types","Which of the following is a Window Object Method?","What tag is used to change the title of a webpage?","What is the correct way to write an Anchor Tag?"]
+var questionHeaders = ["What does RGB stand for?","What are Pseudo Classes?","What are JavaScript Date Types","Which of the following is a Window Object Method?","What tag is used to change the title of a webpage?","What is the correct way to write an reference?"]
 
-var answers1 = [ "bob", "", "", "" ];
-var answers2 = [];
+var answers1 = ["red,blue,green", "red giant branch", "red-blue-green", "robust graphics booster"];
+var answers2 = [":picture-in-picture", ":enable", "::hover", ":what"];
+var answers3 = ["Defined type", "Letter Type", "Noll type", "Symbol type"];
+var answers4 = ["Prompt()", "selection()", "moveTo()", "scrollWith()"];
+var answers5 = ["</title>", "<head>", "<title>", "<page>"];
+var answers6 = ["<a href=('')></a>", "<a href('')></a>", "<a href=()></a>", "<a href=('')><a>"];
 
 
 // set first question text
@@ -23,11 +26,11 @@ function setQuestion(currentQuestion) {
     if (currentQuestion == 0) {
        answerSelection1.textContent = answers1[0];
        answerSelection2.textContent = answers1[1];
-       answerSelection3.textContent = answers1[2];
+       answerSelection3.textContent = answers1[2];// correct answer
        answerSelection4.textContent = answers1[3];
     }
     if (currentQuestion == 1) {
-        answerSelection1.textContent = answers2[0];
+        answerSelection1.textContent = answers2[0];//correct answer
         answerSelection2.textContent = answers2[1];
         answerSelection3.textContent = answers2[2];
         answerSelection4.textContent = answers2[3];
@@ -36,22 +39,22 @@ function setQuestion(currentQuestion) {
         answerSelection1.textContent = answers3[0];
         answerSelection2.textContent = answers3[1];
         answerSelection3.textContent = answers3[2];
-        answerSelection4.textContent = answers3[3];
+        answerSelection4.textContent = answers3[3];//correct answer
     }
     if (currentQuestion == 3) {
         answerSelection1.textContent = answers4[0];
         answerSelection2.textContent = answers4[1];
-        answerSelection3.textContent = answers4[2];
+        answerSelection3.textContent = answers4[2];//correct answer
         answerSelection4.textContent = answers4[3];
     }
     if (currentQuestion == 4) {
         answerSelection1.textContent = answers5[0];
         answerSelection2.textContent = answers5[1];
-        answerSelection3.textContent = answers5[2];
+        answerSelection3.textContent = answers5[2];//correct answer
         answerSelection4.textContent = answers5[3];
     }
     if (currentQuestion == 5) {
-        answerSelection1.textContent = answers6[0];
+        answerSelection1.textContent = answers6[0];//correct answer
         answerSelection2.textContent = answers6[1];
         answerSelection3.textContent = answers6[2];
         answerSelection4.textContent = answers6[3];
@@ -66,11 +69,11 @@ function setQuestion(currentQuestion) {
  // same thing decrease the timer and say it wrong
 
 var generateBtn = document.querySelector("#generate"); //change to the ids in li (answer1)
-generateBtn.addEventListener("click", );
+setQuestion.addEventListener("click", );
 
 //create event listeners for answer buttons
 
-//call setQuestionText using eventlistner to change tp next question
+//call setQuestionText using eventlistner to change to next question
 
 //Think about how to store each answer right or wrong
 
