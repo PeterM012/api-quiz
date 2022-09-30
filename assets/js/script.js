0bbf9b16aeca061919b6f42d6cd580a03f9d7a02
PeterM012 <mpete3d@gmail.com>
2022-09-29 19:02:18 -0700

    add new script lines

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
+var newQuestion = document.getElementById("questions");
+var answerSelection1 = document.getElementById("answers1") 
+var answerSelection2 = document.getElementById("answers2") 
+var answerSelection3 = document.getElementById("answers3") 
+var answerSelection4 = document.getElementById("answers4") 
+
+var currentQuestion = 0; //this will need to be increase by 1
+
+var questionHeaders = ["What does RGB stand for?","What are Pseudo Classes?","What are JavaScript Date Types","Which of the following is a Window Object Method?","What tag is used to change the title of a webpage?","What is the correct way to write an Anchor Tag?"]
+
+var answers1 = [ "bob", "", "", "" ];
+var answers2 = [];
+
+
+// set first question text
+
+function setQuestion(currentQuestion) {
+    
+    // Set question header = questionHeaders[currentQuestion]
+
+    if (currentQuestion == 0) {
+       answerSelection1.textContent = answers1[0];
+       answerSelection2.textContent = answers1[1];
+       answerSelection3.textContent = answers1[2];
+       answerSelection4.textContent = answers1[3];
+    }
+    if (currentQuestion == 1) {
+        
+    }
+}
+ if (answer1 === answer1);
+ // progress the quiz say it right
+
+ if (answer1 === answers2)
+ // same thing decrease the timer and say it wrong
+
+var generateBtn = document.querySelector("#generate"); //change to the ids in li (answer1)
+generateBtn.addEventListener("click", );
+
+//create event listeners for answer buttons
+
+//call setQuestionText using eventlistner to change tp next question
+
+//Think about how to store each answer right or wrong
+
+//How do i tell what button was clicked and compare to the answer
 
 
 
@@ -24,11 +70,3 @@
 
 
 
-
-
-
-
-
-
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", writePassword);
